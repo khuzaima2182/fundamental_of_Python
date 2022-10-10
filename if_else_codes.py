3f15b4aa2fb822cb4988270e68506a4b70ee964c
Khuzaima <ayazkhuzaima6@gmail.com>
2022-10-10 12:45:45 -0700

    Program using if else statements

---
+++ if_else_codes.py
@@ -0,0 +1,15 @@
+### program for even odd
+num = input("Enter a Number = ")
+if(int(num) % 2 == 0):
+    print("Even")
+else:
+    print("Odd")
+
+#program for finding the greatest number
+
+n1 = input("Enter the Number 1 = ")
+n2 = input("Enter the Number 2 = ")
+if(int(n1) > int(n2)):
+    print("First Number is greater")
+else:
+    print("Second Number is greater")
