b5afacecf4f2231bbd9fde9c95ea67605dc27e93
Khuzaima <ayazkhuzaima6@gmail.com>
2022-10-10 12:38:21 -0700

    First program

--- first_program.py
+++ first_program.py
@@ -1,1 +1,12 @@
 print("Hello world")
+
+# Taking input from the user
+num = input("Enter a number = ")
+print(num)
+
+#Simple addition program
+num = input("Enter a number = ")
+num2 = input("Enter a number = ")
+
+addition = int(num)+int(num2)
+print(addition)
