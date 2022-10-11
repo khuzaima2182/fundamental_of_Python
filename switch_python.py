7f0a3095e2bceba12395c6094d7aa535beb3822f
Khuzaima <ayazkhuzaima6@gmail.com>
2022-10-11 11:18:39 -0700

    Switch Statements in python

---
+++ switch_python.py
@@ -0,0 +1,16 @@
+
+perc = input("Enter a Student percentage = ")
+
+if(int(perc) == 60):
+    print("Average")
+elif(int(perc) == 70):
+    print("Good")
+elif(int(perc) == 80):
+    print("Excellent")
+elif (int(perc) == 90):
+    print("Superb")
+else:
+    print("Wrong Input")
+
+
+
