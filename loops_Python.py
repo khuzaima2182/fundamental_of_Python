ac0b041b74c8d53a7964a9d8014459f6f1b6960e
Khuzaima <ayazkhuzaima6@gmail.com>
2022-10-11 11:03:40 -0700

    Program for printing a pattern using for loop

---
+++ loops_Python.py
@@ -0,0 +1,19 @@
+
+for i in range(1,11):
+    print(i)
+
+#Printing a patterns
+
+for i in range(1,10):
+    print(" ")
+    for j in range(10,i,-1):
+        print(" * ",end ='')
+#*  *  *  *  *  *  *  *  *
+#*  *  *  *  *  *  *  *
+#*  *  *  *  *  *  *
+#*  *  *  *  *  *
+#*  *  *  *  *
+#*  *  *  *
+#*  *  *
+#*  *
+#*
